e5ab880a7af719d903cc4cdb127c30f016021934
piustonen <72132818+piustonen@users.noreply.github.com>
2022-12-08 16:42:49 +0200

    Update function.sql

--- function.sql
+++ function.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION clean_no_place()
 RETURNS void
 LANGUAGE plpgsql AS $$
 BEGIN 
-   DELETE FROM releases 
+   DELETE FROM cloud_releases 
    WHERE releases.song_id IN 
       (SELECT releases.song_id from releases where releases.release_place is NULL);
 END;
